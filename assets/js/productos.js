92bcdc8a3368ccabd71b55f182ce0bcec6806b3b
rutvargas <rrebecavargassoller@gmail.com>
2024-01-13 12:50:09 -0500

    cambios para productos

---
+++ assets/js/productos.js
@@ -0,0 +1,10 @@
+
+function mostrarA(){
+    document.getElementById('todos').style.display='none';
+    document.getElementById('seccion-a').style.display='grid';
+}
+
+function mostrarB(){
+    document.getElementById('todos').style.display='none';
+    document.getElementById('seccion-b').style.display='grid';
+}
